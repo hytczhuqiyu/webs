1f4997414f6910495d310b02f51d515e1799cb08
hytczhuqiyu <349712821@qq.com>
2014-08-15 16:55:25 +0800

    8.15下午

--- webQQ/js/index.js
+++ webQQ/js/index.js
@@ -20,4 +20,36 @@ $(function(){
 		}
 	});
 
+	$(".friend").click(function(){
+		var friendId=$(this).attr("friendID");
+		var friendName=$(this).attr("friendName")
+
+		var html="";
+		html += '		<div class="chatArea" >';
+		html += '			<div class="chatheader">';
+		html += '				<div class="chatHfirst"></div>';
+		html += '				<div class="chatName">'+friendName+'</div>';
+		html += '				<div class="chatthird">关闭</div>';
+		html += '			</div>';
+		html += '			<div class="chatcontent"></div>';
+		html += '			<div class="chatFooter">';
+		html += '				<div class="face"></div>';
+		html += '				<textarea class="chattext"></textarea>';
+		html += '				<div class="dianji">发送</div>';
+		html += '			</div>';
+		html += '		</div>';
+
+		$(".wrap").append(html);
+
+		$(".chatthird").click(function(){
+			$(this).parent().parent().remove();
+		});
+
+
+	});
+
+	$(".span32").click(function(){
+		$("*").find(".xx").show();
+
+	});
 });
