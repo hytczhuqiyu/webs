908e0b73ff2d364cdc799180a2066d0a44681c9c
hytczhuqiyu <349712821@qq.com>
2014-08-15 12:03:16 +0800

    8.15上午

--- webQQ/js/index.js
+++ webQQ/js/index.js
@@ -2,6 +2,22 @@ $(function(){
 	$(".XhS").click(function(){
 		$(".XHS").removeClass("active");
 		$(this).addClass("active");
+		var m=$(this).attr("pid");
+		$(".first").removeClass("active");
+		$(".moban"+m).addClass("active");
+	});
+
+
+	$(".menu").click(function(){
+		var a=$(this).find(".menuUl").attr("yon");
+		if(a==0){
+			$(this).find(".menuUl").slideDown();
+			$(this).find(".menuUl").attr("yon",1)
+		}
+		else{
+			$(this).find(".menuUl").slideUp();
+			$(this).find(".menuUl").attr("yon",0)
+		}
 	});
 
 });
