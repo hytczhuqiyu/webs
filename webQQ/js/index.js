546e83c820e0ec925f3ac639f12190699aa30b69
hytczhuqiyu <349712821@qq.com>
2014-08-18 17:15:48 +0800

    8.18

--- webQQ/js/index.js
+++ webQQ/js/index.js
@@ -3,20 +3,20 @@ $(function(){
 		$(".XHS").removeClass("active");
 		$(this).addClass("active");
 		var m=$(this).attr("pid");
-		$(".first").removeClass("active");
-		$(".moban"+m).addClass("active");
+		$(".first").removeClass("active1");
+		$(".moban"+m).addClass("active1");
 	});
 
 
 	$(".menu").click(function(){
-		var a=$(this).find(".menuUl").attr("yon");
+		var a=$(this).next().attr("yon");
 		if(a==0){
-			$(this).find(".menuUl").slideDown();
-			$(this).find(".menuUl").attr("yon",1)
+			$(this).next().slideDown();
+			$(this).next().attr("yon",1)
 		}
 		else{
-			$(this).find(".menuUl").slideUp();
-			$(this).find(".menuUl").attr("yon",0)
+			$(this).next().slideUp();
+			$(this).next().attr("yon",0)
 		}
 	});
 
@@ -34,7 +34,7 @@ $(function(){
 		html += '			<div class="chatcontent"></div>';
 		html += '			<div class="chatFooter">';
 		html += '				<div class="face"></div>';
-		html += '				<textarea class="chattext"></textarea>';
+		html += '				<textarea class="chattext" id="aaaaa"></textarea>';
 		html += '				<div class="dianji">发送</div>';
 		html += '			</div>';
 		html += '		</div>';
@@ -45,11 +45,99 @@ $(function(){
 			$(this).parent().parent().remove();
 		});
 
+		$(".dianji").click(function(){
+			var tt=document.getElementById("aaaaa").value;
+			var liaotian="";
+			liaotian += ' <div class="chatxx">我：'+tt+'</div>';
+			$(".chatcontent").append(liaotian);
+			document.getElementById("aaaaa").value="";
+		});
+
 
 	});
 
 	$(".span32").click(function(){
 		$("*").find(".xx").show();
 
+	});
+
+
+
+	$(".LXsapn").click(function(){
+		$("*").find(".friendUl").hide();
+		$(this).next().show();
+		$("*").find(".LXsapn").removeClass("active2");
+		$(this).addClass("active2");
+	});
+
+
+	$(".list-group").click(function(){
+		var h=$(this).next().attr("yn");
+		if(h==0){
+			$(this).next().show();
+			$(this).next().attr("yn","1");
+			$(this).css("background-image",'url("images/down.png")');
+		}
+		else{
+			$(this).next().hide();
+			$(this).next().attr("yn","0")
+			$(this).css("background-image",'url("images/up.png")');
+		}
+	});
+	var p=25;
+	$(".huantu").click(function(){
+		if(p<30){
+			p=p+1;
+			$("body").css("background-image",'url("images/'+p+'.jpg")');
+			
+		}else{
+			$("body").css("background-image",'url("images/'+p+'.jpg")');
+		}
+	});
+	$(".huantu1").click(function(){
+		if(p>20){
+			p=p-1;
+			$("body").css("background-image",'url("images/'+p+'.jpg")');
+			
+		}else{
+			$("body").css("background-image",'url("images/'+p+'.jpg")');
+		}
+	});
+
+/*状态*/
+	$(".zt1").click(function(){
+		var zt=$(this).attr("wz");
+		$("*").find(".icon1").css("background-position","0px 0px");
+		$(this).parent().hide();
+	});
+	$(".zt2").click(function(){
+		var zt=$(this).attr("wz");
+		$("*").find(".icon1").css("background-position","-72px 0px");
+		$(this).parent().hide();
+	});
+	$(".zt3").click(function(){
+		var zt=$(this).attr("wz");
+		$("*").find(".icon1").css("background-position","-18px 0px");
+		$(this).parent().hide();
+	});
+	$(".zt4").click(function(){
+		var zt=$(this).attr("wz");
+		$("*").find(".icon1").css("background-position","-36px 0px");
+		$(this).parent().hide();
+	});
+	$(".zt5").click(function(){
+		var zt=$(this).attr("wz");
+		$("*").find(".icon1").css("background-position","-108px 0px");
+		$(this).parent().hide();
+	});
+	$(".zt6").click(function(){
+		var zt=$(this).attr("wz");
+		$("*").find(".icon1").css("background-position","-54px 0px");
+		$(this).parent().hide();
+	});
+	$(".zt7").click(function(){
+		var zt=$(this).attr("wz");
+		$("*").find(".icon1").css("background-position","-90px 0px");
+		$(this).parent().hide();
 	});
 });
