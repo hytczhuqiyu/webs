e806e2e6e813c985b5753226df5a72d6a65cc7fa
hytczhuqiyu <349712821@qq.com>
2014-08-21 11:50:45 +0800

    8.21上午

--- weibo/js/weibo.js
+++ weibo/js/weibo.js
@@ -4,4 +4,22 @@ $(function(){
 	},function(){
 		$(".link").css("display","none");
 	});
+
+
+	$(".title").hover(function(){
+		$(this).css("background-color","#FFF");
+		$(this).find(".titleA").css("color","#000");
+		$(this).find(".botlist").show();
+	},function(){
+		$(this).find(".botlist").hide();
+		$(this).css("background-color","");
+		$(this).find(".titleA").css("color","");
+	});
+
+	$(".spright").click(function(){
+		$(this).parent().parent().next().find(".posul").animate({"left":"-380px"},1000);
+	});
+	$(".spleft").click(function(){
+		$(this).parent().parent().next().find(".posul").animate({"left":"-0px"},1000);
+	});
 });
