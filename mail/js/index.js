d59ac71e47f56707188a0b56a6cd8975326149aa
hytczhuqiyu <349712821@qq.com>
2014-08-29 16:50:24 +0800

    8.29下午

--- mail/js/index.js
+++ mail/js/index.js
@@ -7,13 +7,56 @@ $(function(){
 		$("*").find(".inputTxt").attr("placeholder","搜索邮件");
 	});
 	$(".btn").click(function(){
-		var tx=$("*").find(".inputTxt").attr("placeholder");
+		var tx=$("*").find(".inputTxt").val();
 		if(tx==""){
-
-			
 			$("*").find(".inputTxt").attr("placeholder","搜索邮件");
-			alert("请输入关键字");
+			$(".wui_TipInfoBar").show();
+
 		};
+		setTimeout("hide()",2000);
 		return false;
 	});
+
+	/*高级搜索*/
+	$(".more").click(function(){
+		$(".wui_Dialog").show();
+		$(".mb").show();
+	});
+	$(".close").click(function(){
+		$(".wui_Dialog").hide();
+		$(".mb").hide();
+	});
+	$( ".wui_Dialog" ).draggable({ handle: ".head" });
+
+	$(".mailbtn").click(function(){
+		var ttx=$(".iptxt").val();
+		if(ttx==""){
+			$(".wui_TipInfoBar").show();
+
+		};
+		setTimeout("hide()",2000);
+	});
+
+	$(".default2").click(function(){
+		var yon=$(this).attr("yon");
+		if(yon==1){
+			$(this).parent().parent().find(".normal").hide();
+			$(this).attr("yon",0);
+			$(this).parent().find(".ff_nodeopen").css("background-position","-15px 0");
+			$(this).parent().parent().find(".normalLine").hide();
+		}else{
+			$(this).parent().parent().find(".normal").show();
+			$(this).attr("yon",1);
+			$(this).parent().find(".ff_nodeopen").css("background-position","");
+			$(this).parent().parent().find(".normalLine").show();
+		}
+	});
+
+
+
 });
+
+function hide(){
+	$(".wui_TipInfoBar").hide();
+
+}
